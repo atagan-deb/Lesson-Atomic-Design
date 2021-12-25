936a94e73ed2265c8f38ce325de38d298203ab81
atagan-deb <96320896+atagan-deb@users.noreply.github.com>
2021-12-25 13:16:54 +0900

    ページコンポーネントの作成 ユーザー一覧を表示

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "atagan-deb",
-  image: "https://source.unsplash.com/K4mSJ7kc0As",
-  email: "xxx@yyy.zzz",
-  phone: "012-3456-7890",
-  company: {
-    name: "ATomics"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `atagan-deb-${val}`,
+    image: "https://source.unsplash.com/K4mSJ7kc0As",
+    email: "xxx@yyy.zzz",
+    phone: "012-3456-7890",
+    company: {
+      name: "ATomics"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
