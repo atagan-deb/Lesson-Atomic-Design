7119d98c6e83db7b4455add639abe0dcbe8e9ba2
atagan-deb <96320896+atagan-deb@users.noreply.github.com>
2021-12-25 10:22:14 +0900

    organismを実装 ユーザー情報を表示するカードを実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "atagan-deb",
+  image: "https://source.unsplash.com/K4mSJ7kc0As",
+  email: "xxx@yyy.zzz",
+  phone: "012-3456-7890",
+  company: {
+    name: "ATomics"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
