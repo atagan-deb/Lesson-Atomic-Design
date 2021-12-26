5ba612806a2abee4459503c4a4dc072b594d3ea7
atagan-deb <96320896+atagan-deb@users.noreply.github.com>
2021-12-26 09:16:22 +0900

    コンポーネントのmemo化 不要なレンダリングを防ぐ

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,12 +1,11 @@
-import React, { useContext } from "react";
+import React, { memo, useContext } from "react";
 import styled from "styled-components";
 import { UserContext } from "../../../providers/UserProvider";
 
-export const UserIconWithName = (props) => {
+export const UserIconWithName = memo((props) => {
   const { image, name } = props;
   const { userInfo } = useContext(UserContext);
   const isAdmin = userInfo ? userInfo.isAdmin : false;
-
   return (
     <SContainer>
       <SImg height={160} width={160} src={image} alt={name} />
@@ -14,7 +13,7 @@ export const UserIconWithName = (props) => {
       {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
-};
+});
 
 const SContainer = styled.div`
   text-align: center;
